666302f76c2089b4fb9b358e6c0690c472ae3226
syxxzzr <syxxzzr@gmail.com>
2026-06-07 22:31:17 +0800

    refactor(audio): use fftw3f for denoise stats

--- audio_frontend/audio_processor.hpp
+++ audio_frontend/audio_processor.hpp
@@ -4,6 +4,7 @@
 #include "audio_frame.hpp"
 
 #include <cstdint>
+#include <memory>
 #include <vector>
 
 namespace signlang::audio_frontend {
@@ -12,6 +13,7 @@ namespace signlang::audio_frontend {
   public:
     AudioProcessor(AudioFormat input_format, AudioFormat output_format, std::uint32_t publish_period_ms,
                    bool enable_denoise);
+    ~AudioProcessor();
 
     auto output_format() const -> AudioFormat;
     auto output_frame_count() const -> std::uint32_t;
@@ -24,11 +26,14 @@ namespace signlang::audio_frontend {
                      std::uint16_t output_channel_index) const -> std::int32_t;
     void apply_wiener_filter(AudioFrame& output_frame);
 
+    struct DenoiseFftWorkspace;
+
     AudioFormat input_format_;
     AudioFormat output_format_;
     std::uint32_t publish_period_ms_;
     bool enable_denoise_;
     std::vector<std::int16_t> denoise_scratch_;
+    std::unique_ptr<DenoiseFftWorkspace> denoise_fft_workspace_;
   };
 
 } // namespace signlang::audio_frontend
