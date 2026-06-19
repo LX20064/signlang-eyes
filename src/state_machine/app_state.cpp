3357930eb909a71605a3d4cd2690e99096c80a63
syxxzzr <syxxzzr@gmail.com>
2026-06-19 15:24:47 +0800

    feat: add global state machine module

---
+++ src/state_machine/app_state.cpp
@@ -0,0 +1,22 @@
+#include "app_state.hpp"
+
+namespace signlang::state_machine {
+
+  auto default_app_state_key() -> AppStateKey { return AppStateKey{.id = 0}; }
+
+  auto app_state_name(AppState state) -> const char* {
+    switch (state) {
+    case AppState::Normal:
+      return "normal";
+    case AppState::Asr:
+      return "asr";
+    case AppState::SignLanguageChat:
+      return "sign_language_chat";
+    case AppState::SignLanguageAi:
+      return "sign_language_ai";
+    }
+
+    return "unknown";
+  }
+
+} // namespace signlang::state_machine
